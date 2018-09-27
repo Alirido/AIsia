fd95e25c65b18b1aab3e75c2320184f3558a7bd0
Ali Rido <mohalirido@gmail.com>
2018-09-27 10:20:39 +0700

    Modified SimulatedAnnealing.py to handle diff colour

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -13,29 +13,40 @@ class SimulatedAnnealing:
 
 	def run(self):
 		current = self.chessBoard.randomizeBoard()
-		current_h = current.countSameHeuristic()
-		solutionFound = False
-		stop = Decimal('0.05')
-
-		while self.T > stop:
-			if current_h == 0:
-				print("Solution:")
-                print(current.printBoardInfo())
-                self.elapsedTime = self.getElapsedTime()
-                print("Success, Elapsed Time: %sms" % (str(self.elapsedTime)))
-                solutionFound = True
-                break
-            successor = self.chessBoard.randomizeBoard()
-            successor_h = successor.countSameHeuristic()
-            delta_h = successor_h - current_h
-            if delta_h<=0 or math.exp(delta_h / T) > random.uniform(0, 1):
-            	current = successor
-            	current_h = successor_h
-            self.T *= self.ch
+        solutionFound = False
+        stop = Decimal('0.05')
+        if self.chessBoard.isSameColour():
+            current_h = current.countSameHeuristic()
+    
+    		while self.T > stop:
+    			if current_h == 0:
+                    solutionFound = True
+                    break
+                successor = self.chessBoard.randomizeBoard()
+                successor_h = successor.countSameHeuristic()
+                delta_h = successor_h - current_h
+                if (delta_h <= 0) or math.exp(delta_h / T) > random.uniform(0, 1):
+                	current = successor
+                	current_h = successor_h
+                self.T *= self.ch
+        else:
+            current_h = current.countDiffHeuristic() - current.countSameHeuristic()
+            while self.T > stop:
+                successor = self.chessBoard.randomizeBoard()
+                successor_h = successor.countDiffHeuristic() - successor.countSameHeuristic()
+                delta_h = successor_h - current_h
+                if (delta_h > 0) or math.exp(delta_h / T) > random.uniform(0,1):
+                    current = successor
+                    current_h = successor_h
+                self.T *= self.ch
 
         if solutionFound == False:
             self.elapsedTime = self.getElapsedTime()
             print("Unsuccessful, Elapsed Time: %sms" % (str(self.elapsedTime)))
+        else:
+            print(current.printBoardInfo())
+            self.elapsedTime = self.getElapsedTime()
+            print("Success, Elapsed Time: %sms" % (str(self.elapsedTime)))
 
         return self.elapsedTime
 
