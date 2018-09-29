a11ec95d8446a4f1886847012e25cdfb2a84107a
Ali Rido <mohalirido@gmail.com>
2018-09-29 13:28:04 +0700

    Fixed SimulatedAnnealing.py

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -8,22 +8,17 @@ class SimulatedAnnealing:
     def __init__(self, chessBoard):
         self.chessBoard = chessBoard
         self.T = 64*64 # Temperature
-        self.ch = 0.995 # Cooling Schedule
+        self.ch = 0.001 # Cooling Schedule
         self.elapsedTime = 0
         self.startTime = datetime.now()
 
     def run(self):
         self.chessBoard.randomizeBoard()
         current = deepcopy(self.chessBoard)
-        # Below is for debugging purpose
-        current.printBoardInfo()
 
         stop = Decimal('0.05')
         if self.chessBoard.count_black_pieces == 0 or self.chessBoard.count_white_pieces == 0:
             current_h = current.countSameHeuristic()
-            # Belos is for debugging purpose
-            print(current_h)
-            print()
 
             while self.T > stop:
                 if current_h == 0:
@@ -32,10 +27,10 @@ class SimulatedAnnealing:
                 successor = deepcopy(self.chessBoard)
                 successor_h = successor.countSameHeuristic()
                 delta_h = successor_h - current_h
-                if (delta_h <= 0) or math.exp(delta_h / self.T) > random.uniform(0, 1):
+                if (delta_h <= 0) or math.exp(delta_h / self.T) > random.uniform(0.0, 1.0):
                 	current = deepcopy(successor)
                 	current_h = successor_h
-                self.T *= self.ch
+                self.T -= self.ch
         else:
             current_h = current.countDiffHeuristic() - current.countSameHeuristic()
             while self.T > stop:
@@ -43,10 +38,10 @@ class SimulatedAnnealing:
                 successor = deepcopy(self.chessBoard)
                 successor_h = successor.countDiffHeuristic() - successor.countSameHeuristic()
                 delta_h = successor_h - current_h
-                if (delta_h > 0) or math.exp(delta_h / self.T) > random.uniform(0,1):
+                if (delta_h <= 0) or math.exp(delta_h / self.T) > random.uniform(0,1):
                     current = deepcopy(successor)
                     current_h = successor_h
-                self.T *= self.ch
+                self.T -= self.ch
 
         current.printBoardInfo()
         self.elapsedTime = self.getElapsedTime()
