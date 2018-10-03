494a8a82b2750f8c58f661298a76ce351f7c4e47
haniahwafa <haniah_wafa@yahoo.com>
2018-10-03 18:57:29 +0700

    Finalize

--- HillClimbing.py
+++ HillClimbing.py
@@ -1,37 +1,43 @@
 from copy import deepcopy
-
 from ChessBoard import ChessBoard
+
 
 def HillClimbingAlgorithm(chessboard):
     chessboard.randomizeBoard()
     empty_loc = EmptyLocation(chessboard)
     piece_list = Pieces(chessboard)
     successor = BestNeighbor(chessboard, empty_loc, piece_list)
-    while(heuristic(successor) > heuristic(chessboard)):
+    while heuristic(successor) > heuristic(chessboard):
         chessboard = successor
         empty_loc = EmptyLocation(chessboard)
         piece_list = Pieces(chessboard)
         successor = BestNeighbor(chessboard, empty_loc, piece_list)
     return chessboard
 
+
 def EmptyLocation(chessboard):
+    '''
+        return list of empty location on chessboard
+    '''
     empty_loc = []
     for row in range(8):
         for col in range(8):
-            if(chessboard.board[row][col]=={}):
+            if chessboard.board[row][col] == {}:
                 empty_loc.append((row,col))
     return empty_loc
 
+
 def Pieces(chessboard):
+    '''
+        return list of piece on chessboard
+    '''
     piece_list = []
     for row in chessboard.board:
         for piece in row:
-            if(piece!={}):
+            if piece != {}:
                 piece_list.append(piece)
     return piece_list
 
-def heuristic(chessboard):
-    return chessboard.countDiffHeuristic() - chessboard.countSameHeuristic()
 
 def BestNeighbor(chessboard, empty_loc, piece_list):
     max_heuristic = heuristic(chessboard)
@@ -42,7 +48,11 @@ def BestNeighbor(chessboard, empty_loc, piece_list):
             temp_board = deepcopy(chessboard)
             temp_board.movePiece(temp_piece, loc)
             temp_heuristic = heuristic(temp_board)
-            if(temp_heuristic > max_heuristic):
+            if temp_heuristic > max_heuristic:
                 best_board = temp_board
                 max_heuristic = temp_heuristic
     return best_board
+
+
+def heuristic(chessboard):
+    return chessboard.countDiffHeuristic() - chessboard.countSameHeuristic()
